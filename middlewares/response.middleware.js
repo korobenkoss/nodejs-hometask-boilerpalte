3ca670f71bdb2c2c1462b892a2786c943f1d1b4e
Ihor Korobenko <korobenkoss@live.com>
2021-12-25 20:57:14 +0200

    Add authentication logic

--- middlewares/response.middleware.js
+++ middlewares/response.middleware.js
@@ -1,5 +1,7 @@
 const responseMiddleware = (req, res, next) => {
    // TODO: Implement middleware that returns result of the query
+    const { data } = res;
+    res.status(200).json(data);
     next();
 }
 
