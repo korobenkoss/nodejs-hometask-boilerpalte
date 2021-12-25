e345feeee4283fee028e3bcd7faae53f4ec953fe
Ihor Korobenko <korobenkoss@live.com>
2021-12-25 20:08:21 +0200

    Add add user control logic

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,9 +2,37 @@ const { Router } = require('express');
 const UserService = require('../services/userService');
 const { createUserValid, updateUserValid } = require('../middlewares/user.validation.middleware');
 const { responseMiddleware } = require('../middlewares/response.middleware');
+const res = require('express/lib/response');
 
 const router = Router();
 
 // TODO: Implement route controllers for user
+router.get('/api/users', (req, res) => {
+    const allUsers = UserService.searchAll();
+    res.send(allUsers);
+})
+router.get('/api/users/:id', (req, res) => {
+    const oneUser = UserService.search(req.params.id);
+    res.send(oneUser);
+})
+router.post('/api/users', (req, res) => {
+    console.log(req.body)
+    const newUser = req.body;
+    return UserService.addUser(newUser);
+    
+    // res.status(200).send(`Added user ${newUser.firstName} ${newUser.lastName}`);
+})
+router.put('/api/users/:id', (req, res) => {
+    const userIdToUpdate = req.params.id;
+    const userDataToUpdate = req.body;
+    UserService.updateUser(userIdToUpdate, userDataToUpdate);
+    res.status(200).send(`Updated user with id: ${userIdToUpdate}`)
+})
+router.delete('/api/users/:id', (req, res) => {
+    const userIdToDelete = req.params.id;
+    UserService.deleteUser(userIdToDelete);
+    res.status(200).send(`Deleted user with id: ${userIdToDelete}`)
+})
 
 module.exports = router;
+
--- services/userService.js
+++ services/userService.js
@@ -4,12 +4,43 @@ class UserService {
 
     // TODO: Implement methods to work with user
 
+    // get a single user
     search(search) {
         const item = UserRepository.getOne(search);
         if(!item) {
             return null;
         }
         return item;
+    }
+
+    // get all users
+    searchAll() {
+        const allItems = UserRepository.getAll();
+        if(!allItems) {
+            return null;
+        }
+        return allItems;
+    }
+
+    // add a user
+    addUser(usr) {
+        const addUserSuccess = UserRepository.create(usr);
+
+        return addUserSuccess;
+    }
+
+    // update a user
+    updateUser(id, data) {
+        const updateUserSuccess = UserRepository.update(id, data);
+
+        return updateUserSuccess;
+    }
+
+    // delete a user
+    deleteUser(id) {
+        const deleteUserSuccess = UserRepository.delete(id);
+
+        return deleteUserSuccess;
     }
 }
 
