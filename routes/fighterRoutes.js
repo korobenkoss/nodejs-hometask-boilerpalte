94dd9621df7ab8279833bb13e2c3b78c07df8a59
Ihor Korobenko <korobenkoss@live.com>
2021-12-25 20:41:36 +0200

    Add fighter control logic

--- routes/fighterRoutes.js
+++ routes/fighterRoutes.js
@@ -6,5 +6,28 @@ const { createFighterValid, updateFighterValid } = require('../middlewares/fight
 const router = Router();
 
 // TODO: Implement route controllers for fighter
+router.get("api/fighters/:id", (req, res) => {
+    const oneFighter = FighterService.search(req.params.id);
+    res.send(oneFighter);
+});
+router.get("api/fighters", (req, res) => {
+    const allFighters = FighterService.searchAll();
+    res.send(allFighters);
+});
+router.post("api/fighters", (req, res) => {
+    const newFighter = req.body;
+    return FighterService.addFighter(newFighter);
+});
+router.put("api/fighters/:id", (req, res) => {
+    const fighterIdToUpdate = req.params.id;
+    const fighterDataToUpdate = req.body;
+    FighterService.updateFighter(fighterIdToUpdate, fighterDataToUpdate);
+    res.status(200).send(`Updated fighter with id: ${fighterIdToUpdate}`)
+});
+router.delete("api/fighters/:id", (req, res) => {
+    const fighterIdToDelete = req.params.id;
+    FighterService.deleteFighter(fighterIdToDelete);
+    res.status(200).send(`Deleted fighter with id: ${fighterIdToDelete}`);
+});
 
 module.exports = router;
--- services/fighterService.js
+++ services/fighterService.js
@@ -2,6 +2,43 @@ const { FighterRepository } = require('../repositories/fighterRepository');
 
 class FighterService {
     // TODO: Implement methods to work with fighters
+    
+    // get one fighter
+    search(fighter) {
+        const item = FighterRepository.getOne(fighter);
+        if(!item) return null;
+
+        return item;
+    }
+
+    // get all fighters
+    searchAll() {
+        const allItems = FighterRepository.getAll();
+        if(!allItems) return null;
+
+        return allItems;
+    }
+
+    // create fighter
+    addFighter(fighter) {
+        const addFighterSuccess = FighterRepository.create(fighter);
+
+        return addFighterSuccess;
+    }
+
+    // update fighter
+    updateFighter(id, data) {
+        const updateFighterSuccess = FighterRepository.update(id, data);
+
+        return updateFighterSuccess;
+    }
+
+    // delete fighter
+    deleteFighter(id) {
+        const deleteFighterSuccess = FighterRepository.delete(id)
+
+        return deleteFighterSuccess;
+    }
 }
 
 module.exports = new FighterService();
